7aac04d783c516547422509a3a490464612b48f3
RazM1213 <razm1213@gmail.com>
2022-08-18 11:37:29 +0300

    changed request params

--- send/request.py
+++ send/request.py
@@ -1,9 +1,8 @@
-import string
 from typing import List
 
 
 class Request:
-    def __init__(self, request_method: str, url: str, status_codes: List[string], request_body: dict = None, request_headers: dict = None):
+    def __init__(self, request_method: str, url: str, status_codes: List[str], request_body: dict = None, request_headers: dict = None):
         self.request_method = request_method
         self.url = url
         self.status_codes = status_codes
