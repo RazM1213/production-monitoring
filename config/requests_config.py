c47ec5ea141b7638303c2be05e5b130e2bf18cdf
RazM1213 <razm1213@gmail.com>
2022-08-18 11:55:39 +0300

    added success status codes config

--- config/requests_config.py
+++ config/requests_config.py
@@ -8,6 +8,8 @@ TODO_GET_ROUTE = "https://localhost:44302/"
 
 POSSIBLE_STATUS_CODES = [200, 400, 404, 500]
 
+SUCCESS_STATUS_CODES = [200, 201, 202, 203, 204]
+
 HTTP_GET = "get"
 
 HTTP_POST = "post"
