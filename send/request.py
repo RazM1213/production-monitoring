e17cf9023659a1491c62a4c0b22c414c8f7b7711
RazM1213 <razm1213@gmail.com>
2022-08-18 11:33:40 +0300

    set request body and headers default values

--- send/request.py
+++ send/request.py
@@ -3,7 +3,7 @@ from typing import List
 
 
 class Request:
-    def __init__(self, request_method: str, url: str, status_codes: List[string], request_body: dict, request_headers: dict):
+    def __init__(self, request_method: str, url: str, status_codes: List[string], request_body: dict = None, request_headers: dict = None):
         self.request_method = request_method
         self.url = url
         self.status_codes = status_codes
