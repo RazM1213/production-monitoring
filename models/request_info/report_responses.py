45b0dc6ad06b35e404712421d8414219a39a4b2e
RazM1213 <razm1213@gmail.com>
2022-08-21 15:42:06 +0300

    modified ReportResponses to a dataclass

--- models/request_info/report_responses.py
+++ models/request_info/report_responses.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
 from typing import List, Dict
 
@@ -7,18 +7,14 @@ from typing import List, Dict
 class ErrorRequest:
     position: int
     content: str
+    
 
-
+@dataclass
 class ReportResponses:
-    def __init__(self, status_codes: List[int], request_amount: int = 0):
-        self.time = datetime.now()
-        self.status_codes: Dict[int, int] = {}
-        self.request_amount = request_amount
-        self.request_times = []
-        self.error_requests_info: Dict[int, List[ErrorRequest]] = {}
-        self.is_failed: bool = False
-        self.error_count: int = 0
-        for status_code in status_codes:
-            self.status_codes[status_code] = 0
-            if status_code / 100 != 2:
-                self.error_requests_info[status_code] = []
+    request_amount: int
+    status_codes: Dict[int, int] = field(default_factory=lambda: {})
+    error_requests_info: Dict[int, List[ErrorRequest]] = field(default_factory=lambda: {})
+    is_failed: bool = False
+    error_count: int = 0
+    time: datetime = datetime.now()
+    request_times: List[int] = field(default_factory=lambda: [])
