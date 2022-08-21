446c00b1c4803629eab6e9c5d90367d9acb03f4e
RazM1213 <razm1213@gmail.com>
2022-08-21 16:31:05 +0300

    deleted get_run_stat method for it is redundant

--- transform/response/response_transformer.py
+++ transform/response/response_transformer.py
@@ -1,17 +1,17 @@
-from typing import List
+from typing import List, Dict
 
 from requests import Response
 
-from models.elastic.elastic_report_response_doc import RunStat
 from models.elastic.request_time import RequestTime
 from models.elastic.status_code_counter import StatusCodeCounter
 from models.request_info.report_responses import ReportResponses
+from models.request_info.response_values import ResponseValues
 
 
 class ResponseTransformer:
     @staticmethod
-    def get_run_stat(env_name: str) -> RunStat:
-        return RunStat(env_name)
+    def get_results_dict(routes: List[str]) -> Dict[str: str]:
+        pass
 
     @staticmethod
     def get_status_code_info(status_code_list: List[StatusCodeCounter], status_code: int) -> [List[StatusCodeCounter]]:
@@ -20,12 +20,12 @@ class ResponseTransformer:
     @staticmethod
     def get_status_code_counter(status_code: int, count: int) -> StatusCodeCounter:
         pass
-    
+
     @staticmethod
     def get_request_time(responses: ReportResponses) -> List[RequestTime]:
         request_times = list(map(lambda time: time.total_seconds(), responses.request_times))
         return RequestTime(sum(request_times) / len(request_times), max(request_times), min(request_times))
 
     @staticmethod
-    def get_response_values(response: Response):
+    def get_response_values(response: Response) -> ResponseValues:
         pass
