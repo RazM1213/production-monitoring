19b6346c0fa804410720fc804cb4d27b31844a8f
RazM1213 <razm1213@gmail.com>
2022-08-21 10:36:23 +0300

    modified Monitor

--- monitor.py
+++ monitor.py
@@ -1,14 +1,12 @@
-from build.report.report_builder import ReportBuilder
 from publish.i_publisher import IPublisher
 from send.request_sender import RequestSender
 from transform.response.response_transformer import ResponseTransformer
 
 
 class Monitor:
-    def __init__(self, request_sender: RequestSender, response_transformer: ResponseTransformer, report_builder: ReportBuilder, publisher: IPublisher):
-        self.request_sender = request_sender
-        self.response_transformer = response_transformer
-        self.report_builder = report_builder
+    def __init__(self, publisher: IPublisher):
+        self.request_sender = RequestSender()
+        self.response_transformer = ResponseTransformer()
         self.publisher = publisher
 
     def start(self):
