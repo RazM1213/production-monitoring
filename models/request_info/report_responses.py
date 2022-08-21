986d65b0286d52c523d0a3a112e6298090adf7ee
RazM1213 <razm1213@gmail.com>
2022-08-21 10:34:34 +0300

    modified ReportResponses to be data class

--- models/request_info/report_responses.py
+++ models/request_info/report_responses.py
@@ -9,16 +9,12 @@ class ErrorRequest:
     content: str
 
 
+@dataclass
 class ReportResponses:
-    def __init__(self, status_codes: List[int], request_amount: int = 0):
-        self.time = datetime.now()
-        self.status_codes: Dict[int, int] = {}
-        self.request_amount = request_amount
-        self.request_times = []
-        self.error_requests_info: Dict[int, List[ErrorRequest]] = {}
-        self.is_failed = False
-        self.error_count = 0
-        for status_code in status_codes:
-            self.status_codes[status_code] = 0
-            if status_code / 100 != 2:
-                self.error_requests_info[status_code] = []
+    time: datetime.now()
+    request_amount: int
+    request_times: List
+    status_codes: Dict[int, int]
+    error_requests_info: Dict[int, List[ErrorRequest]]
+    is_failed: bool = False
+    error_count: int = 0
