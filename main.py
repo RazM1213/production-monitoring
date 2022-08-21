02bb79f4dd4c69575b2863f65c94f7f8d88ab6c3
RazM1213 <razm1213@gmail.com>
2022-08-21 10:38:21 +0300

    modified main

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from build.report.report_builder import ReportBuilder
 from config.kafka_config import TODO_TOPIC, LOCALHOST_SERVER
 from monitor import Monitor
 from publish.kafka.kafka_publisher import KafkaPublisher
-from send.request_sender import RequestSender
-from transform.response.response_transformer import ResponseTransformer
 
 
 def main():
     monitor = Monitor(
-        RequestSender(),
-        ResponseTransformer(),
-        ReportBuilder(),
         KafkaPublisher(topic=TODO_TOPIC, bootstrap_servers=[LOCALHOST_SERVER])
     )
 
