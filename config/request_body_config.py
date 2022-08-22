2889e8079f5c6ffa9a0b2f7c1d7a32be7c9a95b2
RazM1213 <razm1213@gmail.com>
2022-08-22 14:53:37 +0300

    added request_body_config.py and url_config.py

---
+++ config/request_body_config.py
@@ -0,0 +1,6 @@
+POST_REQUEST_BODY = {
+    "todoId": 0,
+    "listId": 1882,
+    "todoName": "Monitor",
+    "finishDate": "2022-08-22T11:44:07.845Z"
+}
---
+++ config/url_config.py
@@ -0,0 +1,3 @@
+IS_ALIVE_URL = "https://localhost:44302/status/isalive"
+
+API_TODOS_URL = "https://localhost:44302/api/Todos"
