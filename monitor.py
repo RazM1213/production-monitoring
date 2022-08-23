a5825c15f3e9a935a1a1322e5f41e8d4b4f10d51
RazM1213 <razm1213@gmail.com>
2022-08-23 12:24:54 +0300

    removed redundant if clause

--- monitor.py
+++ monitor.py
@@ -1,13 +1,11 @@
-from config.config import TOPIC, BOOTSTRAP_SERVERS
+import threading
+
 from http_methods.http_methods_enum import HttpMethodsEnum
 from publish.i_publisher import IPublisher
-from publish.kafka.kafka_publisher import KafkaPublisher
 from send.get_request_sender import GetRequestSender
 from send.post_request_sender import PostRequestSender
-from send.request_sender import RequestSender
 from todo_api_monitor_request_mapping import REQUESTS
 from transform.response.response_transformer import ResponseTransformer
-import threading
 
 
 class Monitor:
@@ -31,8 +29,3 @@ class Monitor:
 
     def start(self):
         self.send_requests_async()
-
-
-if __name__ == "__main__":
-    monitor = Monitor(KafkaPublisher(TOPIC, BOOTSTRAP_SERVERS))
-    monitor.start()
