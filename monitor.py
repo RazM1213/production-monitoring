ed0bc2bd6d2794aa7ef18d7c01533ffa65f4e728
RazM1213 <razm1213@gmail.com>
2022-08-24 16:48:14 +0300

    implemented Monitor.start()

--- monitor.py
+++ monitor.py
@@ -1,25 +1,41 @@
+import queue
 import threading
+import time
+from typing import List, Dict
 
 from http_methods import http_method_func_mapping
+from models.request_info.response_values import ResponseValues
 from publish.i_publisher import IPublisher
+from send.request import Request
 from transform.response.response_transformer import ResponseTransformer
 
 
 class Monitor:
-    def __init__(self, requests: dict, publisher: IPublisher):
+    def __init__(self, requests: Dict[str, Request], publisher: IPublisher):
         self.requests = requests
         self.response_transformer = ResponseTransformer()
         self.publisher = publisher
 
-    def send_requests_async(self):
+    def send_requests_async(self, route_name: str) -> List[ResponseValues]:
         threads = []
-        for request in self.requests:
-            for request_index in range(self.requests[request].amount):
-                sender = http_method_func_mapping.HTTP_METHODS_FUNCS[self.requests[request].request_method.value]
-                threads.append(threading.Thread(target=sender, args=[self.requests[request]]))
+        response_values = queue.Queue()
+        for request_index in range(self.requests[route_name].amount):
+            sender = http_method_func_mapping.HTTP_METHODS_FUNCS[self.requests[route_name].request_method.value]
+            threads.append(threading.Thread(target=sender, args=([self.requests[route_name]], response_values)))
 
         for thread in threads:
             thread.start()
+        time.sleep(0.3)
+        return list(response_values.queue)
 
     def start(self):
-        self.send_requests_async()
+        reports = []
+        for route_name in self.requests:
+            reports.append(self.response_transformer.get_elastic_report_doc(
+                route_name,
+                self.response_transformer.get_report_responses(self.send_requests_async(route_name))
+                )
+            )
+
+        for report in reports:
+            self.publisher.publish(report)
