bbec5c42e474e0ca76c547e830c95fba480230f0
RazM1213 <razm1213@gmail.com>
2022-09-01 10:14:12 +0300

    modified config to be more generic

--- todo_api_monitor_request_mapping.py
+++ todo_api_monitor_request_mapping.py
@@ -1,26 +1,26 @@
-from config.config import IS_ALIVE_URL, API_TODOS_URL, POST_REQUEST_BODY
+from config.config import POST_REQUEST_BODY, BASE_URL, ROUTE_1, ROUTE_2
 from consts.status_codes import ALL_STATUS_CODES
-from utils.http_methods.http_methods_enum import HttpMethodsEnum
 from send.request import Request
+from utils.http_methods.http_methods_enum import HttpMethodsEnum
 
 REQUESTS = {
     "is_alive": Request(
         request_method=HttpMethodsEnum.GET,
-        url=IS_ALIVE_URL,
+        url=BASE_URL + ROUTE_1,
         status_codes=ALL_STATUS_CODES,
         amount=10
     ),
-    # "get_all": Request(
-    #     request_method=HttpMethodsEnum.GET,
-    #     url=API_TODOS_URL,
-    #     status_codes=ALL_STATUS_CODES,
-    #     amount=5
-    # ),
-    # "post_todo": Request(
-    #     request_method=HttpMethodsEnum.POST,
-    #     url=API_TODOS_URL,
-    #     status_codes=ALL_STATUS_CODES,
-    #     request_body=POST_REQUEST_BODY,
-    #     amount=2
-    # )
+    "get_all": Request(
+        request_method=HttpMethodsEnum.GET,
+        url=BASE_URL + ROUTE_2,
+        status_codes=ALL_STATUS_CODES,
+        amount=5
+    ),
+    "post_todo": Request(
+        request_method=HttpMethodsEnum.POST,
+        url=BASE_URL + ROUTE_2,
+        status_codes=ALL_STATUS_CODES,
+        request_body=POST_REQUEST_BODY,
+        amount=2
+    )
 }
