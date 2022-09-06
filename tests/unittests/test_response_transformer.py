be2b08803826a8b270222eff3f6e78ac2ea9475b
RazM1213 <razm1213@gmail.com>
2022-09-06 15:28:32 +0300

    implemented test_invalid_report_responses_error_contents

--- tests/unittests/test_response_transformer.py
+++ tests/unittests/test_response_transformer.py
@@ -22,16 +22,6 @@ class TestResponseTransformer(unittest.TestCase):
         self.assertEqual(report_responses.error_count, 0)
         self.assertEqual(report_responses.request_times, [datetime.timedelta(microseconds=1000)])
 
-    def test_invalid_get_report_responses_empty_list(self):
-        # Arrange
-        responses_values = []
-
-        # Act
-        report_responses = ResponseTransformer.get_report_responses(responses_values=responses_values)
-
-        # Assert
-        self.assertEqual(report_responses, None)
-
     def test_valid_get_report_responses_more_than_one_item_list(self):
         # Arrange
         responses_values = [ResponseValues(timedelta(microseconds=1000), 200), ResponseValues(timedelta(microseconds=1000), 404)]
@@ -44,6 +34,28 @@ class TestResponseTransformer(unittest.TestCase):
         self.assertEqual(report_responses.status_codes, {200: 1, 404: 1})
         self.assertEqual(report_responses.is_failed, True)
         self.assertEqual(report_responses.error_count, 1)
+
+    def test_invalid_get_report_responses_empty_list(self):
+        # Arrange
+        responses_values = []
+
+        # Act
+        report_responses = ResponseTransformer.get_report_responses(responses_values=responses_values)
+
+        # Assert
+        self.assertEqual(report_responses, None)
+
+    def test_invalid_report_responses_error_contents(self):
+        # Arrange
+        responses_values = [ResponseValues(timedelta(microseconds=1000), 404, "Error Test")]
+
+        # Act
+        report_responses = ResponseTransformer.get_report_responses(responses_values=responses_values)
+
+        # Assert
+        self.assertEqual(report_responses.error_count, 1)
+        self.assertEqual(report_responses.error_requests_info[404][0].content, "Error Test")
+        self.assertEqual(report_responses.is_failed, True)
 
     def test_get_elastic_report_doc(self):
         pass
