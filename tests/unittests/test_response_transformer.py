e7493f4536fc4a848baf1da964b10cfc0b6efa92
RazM1213 <razm1213@gmail.com>
2022-09-06 15:35:57 +0300

    implemented test_invalid_get_report_responses_no_status_codes

--- tests/unittests/test_response_transformer.py
+++ tests/unittests/test_response_transformer.py
@@ -45,7 +45,7 @@ class TestResponseTransformer(unittest.TestCase):
         # Assert
         self.assertEqual(report_responses, None)
 
-    def test_invalid_report_responses_error_contents(self):
+    def test_invalid_get_report_responses_error_contents(self):
         # Arrange
         responses_values = [ResponseValues(timedelta(microseconds=1000), 404, "Error Test")]
 
@@ -56,6 +56,16 @@ class TestResponseTransformer(unittest.TestCase):
         self.assertEqual(report_responses.error_count, 1)
         self.assertEqual(report_responses.error_requests_info[404][0].content, "Error Test")
         self.assertEqual(report_responses.is_failed, True)
+
+    def test_invalid_get_report_responses_no_status_codes(self):
+        # Arrange
+        responses_values = [ResponseValues(timedelta(microseconds=1000))]
+
+        # Act
+        report_responses = ResponseTransformer.get_report_responses(responses_values=responses_values)
+
+        # Assert
+        self.assertEqual(report_responses, None)
 
     def test_get_elastic_report_doc(self):
         pass
