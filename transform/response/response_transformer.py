f0ece991b4aa23d53cb88a1e174b1a127f6f6234
RazM1213 <razm1213@gmail.com>
2022-09-06 15:13:06 +0300

    modified get_report_responses to handle a situation where there are no requests made

--- transform/response/response_transformer.py
+++ transform/response/response_transformer.py
@@ -16,6 +16,8 @@ class ResponseTransformer:
     @staticmethod
     def get_report_responses(responses_values: List[ResponseValues]) -> ReportResponses:
         report_responses = ReportResponses(request_amount=len(responses_values))
+        if report_responses.request_amount == 0:
+            return None
         response_values_index = 0
         for response_values in responses_values:
             if response_values.status_code in report_responses.status_codes:
