6a4d9a4336efac1524c43f618ab82d38ea0f6553
RazM1213 <razm1213@gmail.com>
2022-09-06 14:59:41 +0300

    async fix final commit

--- todo_api_monitor_request_mapping.py
+++ todo_api_monitor_request_mapping.py
@@ -1,4 +1,4 @@
-from config.config import BASE_URL, ROUTE_1
+from config.config import BASE_URL, ROUTE_1, ROUTE_2, POST_REQUEST_BODY
 from consts.status_codes import ALL_STATUS_CODES
 from send.request import Request
 from utils.http_methods.http_methods_enum import HttpMethodsEnum
@@ -10,17 +10,17 @@ REQUESTS = {
         status_codes=ALL_STATUS_CODES,
         amount=10
     ),
-    # ROUTE_2: Request(
-    #     request_method=HttpMethodsEnum.GET,
-    #     url=BASE_URL + ROUTE_2,
-    #     status_codes=ALL_STATUS_CODES,
-    #     amount=5
-    # ),
-    # ROUTE_2: Request(
-    #     request_method=HttpMethodsEnum.POST,
-    #     url=BASE_URL + ROUTE_2,
-    #     status_codes=ALL_STATUS_CODES,
-    #     request_body=POST_REQUEST_BODY,
-    #     amount=10
-    # )
+    f"GET {ROUTE_2}": Request(
+        request_method=HttpMethodsEnum.GET,
+        url=BASE_URL + ROUTE_2,
+        status_codes=ALL_STATUS_CODES,
+        amount=5
+    ),
+    f"POST {ROUTE_2}": Request(
+        request_method=HttpMethodsEnum.POST,
+        url=BASE_URL + ROUTE_2,
+        status_codes=ALL_STATUS_CODES,
+        request_body=POST_REQUEST_BODY,
+        amount=10
+    )
 }
