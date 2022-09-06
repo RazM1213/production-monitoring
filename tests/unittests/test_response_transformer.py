d5fccfa0e4b707ddcd213e777336a5078ce7566f
RazM1213 <razm1213@gmail.com>
2022-09-06 15:12:02 +0300

    added unittests directory and first unittest file

---
+++ tests/unittests/test_response_transformer.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class TestResponseTransformer(unittest.TestCase):
+    def test_valid_get_report_responses_one_item_list(self):
+        pass
+
+    def test_invalid_get_report_responses_empty_list(self):
+        pass
+
+    def test_valid_get_report_responses_more_than_one_item_list(self):
+        pass
+
+    def test_get_elastic_report_doc(self):
+        pass
+
+    def test_get_status_code_info(self):
+        pass
+
+    def test_get_request_time(self):
+        pass
