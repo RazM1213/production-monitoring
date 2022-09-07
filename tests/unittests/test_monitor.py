822baf90218687841fb80454d533087f2164829d
RazM1213 <razm1213@gmail.com>
2022-09-07 11:12:06 +0300

    added test_monitor.py to test Monitor class

---
+++ tests/unittests/test_monitor.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestMonitor(unittest.TestCase):
+    def test_send_requests_async(self):
+        pass
+
+    def test_get_contents(self):
+        pass
+
+    def test_get_responses_values(self):
+        pass
+   
