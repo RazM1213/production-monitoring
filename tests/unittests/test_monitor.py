8c55ad00eefbeef1e5fa9a8791adf06da0710d88
RazM1213 <razm1213@gmail.com>
2022-09-07 11:58:09 +0300

    added setup to TestMonitor

--- tests/unittests/test_monitor.py
+++ tests/unittests/test_monitor.py
@@ -1,7 +1,30 @@
 import unittest
 
+from config.config import BASE_URL, ROUTE_1
+from consts.status_codes import ALL_STATUS_CODES
+from monitor import Monitor
+from publish.kafka.kafka_publisher import KafkaPublisher
+from send.request import Request
+from utils.http_methods.http_methods_enum import HttpMethodsEnum
+
 
 class TestMonitor(unittest.TestCase):
+    REQUESTS = {
+        "test_route_1": Request(
+            request_method=HttpMethodsEnum.GET,
+            url=BASE_URL + ROUTE_1,
+            status_codes=ALL_STATUS_CODES,
+            amount=10
+        )
+    }
+
+    BOOTSTRAP_SERVERS = ["localhost:9092"]
+    TOPIC = "PMTestTopic"
+    PUBLISHER = KafkaPublisher(bootstrap_servers=BOOTSTRAP_SERVERS, topic=TOPIC)
+
+    def setUp(self):
+        self.monitor = Monitor(self.REQUESTS, self.PUBLISHER)
+
     def test_send_requests_async(self):
         pass
 
@@ -10,4 +33,3 @@ class TestMonitor(unittest.TestCase):
 
     def test_get_responses_values(self):
         pass
-   
